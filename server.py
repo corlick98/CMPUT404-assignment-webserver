7cd7df3366376dc0f312da65310304969f86d2c4
Christopher Orlick <corlick@ualberta.ca>
2022-09-27 23:17:42 -0600

    Should be functional, needs commenting/cleaning

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 #  coding: utf-8 
 import socketserver
+import os
 
-# Copyright 2013 Abram Hindle, Eddie Antonio Santos
+# Copyright 2013 Abram Hindle, Eddie Antonio Santos, Christopher Orlick
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -25,14 +26,88 @@ import socketserver
 # run: python freetests.py
 
 # try: curl -v -X GET http://127.0.0.1:8080/
+def get_request_line(request_string):
+    return request_string.split()[0:3]
 
+def path_is_legal(parent_path, child_path):
+    # Credit to https://stackoverflow.com/a/37095733
+    # Smooth out relative path names, note: if you are concerned about symbolic links, you should use os.path.realpath too
+    parent_path = os.path.abspath(parent_path)
+    child_path = os.path.abspath(child_path)
+
+    # Compare the common path of the parent and child path with the common path of just the parent path. 
+    # Using the commonpath method on just the parent path will regularise the path name in the same way 
+    # as the comparison that deals with both paths, removing any trailing path separator
+    return os.path.commonpath([parent_path]) == os.path.commonpath([parent_path, child_path])
+
+def result_405():
+    return """HTTP/1.1 405 Method Not Allowed\r\nContent-Type: text/html\r\nConnection: Closed\r\n
+<html>
+    <head>
+        <title>405 Method Not Allowed</title>
+    </head>
+    <body>
+        <h1>Method Not Allowed</h1>
+    </body>
+</html>"""
+
+def result_404():
+    return """HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\nConnection: Closed\r\n
+<html>
+    <head>
+        <title>404 Not Found</title>
+    </head>
+    <body>
+        <h1>Not Found</h1>
+    </body>
+</html>"""
+
+def result_301(new_loc):
+    return """HTTP/1.1 301 Moved Permanently\r\nContent-Type: text/html\r\nConnection: Closed\r\nLocation: {new_loc}\r\n
+<html>
+    <head>
+        <title>301 Moved Permanently</title>
+    </head>
+    <body>
+        <h1>Moved Permanently<h1>
+        The document has moved <a href="{new_loc}">here</a>.
+    </body>
+</html>
+""".format(new_loc=new_loc)
+
+def result_200(path):
+    file_type = path.split('.')[-1].split('/')[0]
+    file_size = os.path.getsize(path)
+    with open(path, 'r') as f:
+        header = "HTTP/1.1 200 OK\r\nContent-Type: text/{}\r\nContent-Size: {}\r\nConnection: Closed\r\n\n".format(file_type, file_size)
+        return header + f.read()
+
+
+
+def _handler(method, path):
+    if method != 'GET':
+        return result_405()
+    current_location = os.path.dirname(os.path.abspath(__file__))+'/www'
+    full_path = current_location + path
+    if not path_is_legal(current_location, full_path):
+        return result_404()
+    if os.path.isfile(full_path):
+        return result_200(full_path)
+    elif os.path.isdir(full_path):
+        if full_path[-1] == "/": #return index.html
+            return result_200(full_path+"index.html")
+        else:
+            return result_301(path+"/")
+    return result_404()
 
 class MyWebServer(socketserver.BaseRequestHandler):
     
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        data_string = self.data.decode()
+        method, path, version = get_request_line(data_string)
+        response = _handler(method, path)
+        self.request.sendall(bytearray(response,'utf-8'))
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
